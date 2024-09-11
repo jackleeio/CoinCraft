346e80999b3f3f6f3d7f8416f020754e08f44347
Jack Lee <280147597@qq.com>
2024-09-11 11:35:30 +0800

    add ERC20WithBurnable

--- src/ERC20/ERC20FixedSupply.sol
+++ src/ERC20/ERC20FixedSupply.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-//固定总量代币
+// Fixed supply token
 contract ERC20FixedSupply is ERC20 {
     constructor(
-        string memory name, //代币名称
-        string memory symbol, //代币缩写
-        uint8 decimals, //精度
-        uint256 totalSupply //发行总量
+        string memory name, // Token name
+        string memory symbol, // Token symbol
+        uint8 decimals, // Decimals
+        uint256 totalSupply // Total supply
     ) ERC20(name, symbol) {
         _mint(msg.sender, totalSupply * (10**uint256(decimals)));
     }
---
+++ src/ERC20/ERC20WithBurnable.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+// Burnable token
+contract ERC20WithBurnable is ERC20, ERC20Burnable {
+    // Constructor for the burnable ERC20 token
+    // Initializes the token with a name, symbol, decimals, and total supply
+    constructor(
+        string memory name,      // The name of the token
+        string memory symbol,    // The symbol of the token
+        uint8 decimals,          // The number of decimals for the token
+        uint256 totalSupply      // The total supply of the token
+    ) ERC20(name, symbol) {
+        _mint(msg.sender, totalSupply * (10**uint256(decimals))); // Mint the total supply to the contract deployer
+    }
+}
