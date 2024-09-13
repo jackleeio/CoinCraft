144cd27f98697802121dbfd342d0737b9f0a93a2
Jack Lee <280147597@qq.com>
2024-09-13 13:34:42 +0800

    update ERC20WithPausable

--- src/ERC20/ERC20WithPausable.sol
+++ src/ERC20/ERC20WithPausable.sol
@@ -5,15 +5,17 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // Importing the ERC20Pausable extension for pausable token functionality
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+// Importing the Ownable contract for pausable token functionality
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-// ERC20WithPausable contract that extends ERC20 and ERC20Pausable
-contract ERC20WithPausable is ERC20, ERC20Pausable {
+// ERC20WithPausable contract that extends ERC20, ERC20Pausable, and Ownable
+contract ERC20WithPausable is ERC20, ERC20Pausable, Ownable {
     // Constructor to initialize the token with name, symbol, and total supply
     constructor(
         string memory name,
         string memory symbol,
         uint256 totalSupply
-    ) ERC20(name, symbol) {
+    ) ERC20(name, symbol) Ownable(msg.sender) {
         // Minting the total supply to the contract deployer
         _mint(msg.sender, totalSupply * 10 ** decimals());
     }
@@ -26,5 +28,21 @@ contract ERC20WithPausable is ERC20, ERC20Pausable {
     ) internal override(ERC20, ERC20Pausable) {
         // Calling the _update function from ERC20Pausable
         ERC20Pausable._update(from, to, value);
+    }
+
+    /**
+     * @dev Pauses the contract, preventing token transfers.
+     * Only callable by the owner.
+     */
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @dev Unpauses the contract, allowing token transfers.
+     * Only callable by the owner.
+     */
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
